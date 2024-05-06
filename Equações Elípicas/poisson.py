31b0290c5579405994521e7c53305c3ecb789432
Callebez <callebe.rocha@outlook.com>
2024-05-05 22:05:37 -0300

    final adjustments for creating the report

--- Equações Elípicas/poisson.py
+++ Equações Elípicas/poisson.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from scipy.sparse import *
 from scipy.sparse.linalg import *
+from matplotlib import cm
 
 def matprint(mat, fmt="g"):
     col_maxes = [max([len(("{:"+fmt+"}").format(x)) for x in col]) for col in mat.T]
@@ -51,11 +52,6 @@ def analyticalSol(r, theta, n):
 
 
 
-# fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')
-
-# # ax.plot_surface(R, T, sol)
-
 # A = Ablocks(nr,nt)
 # # matprint(A.toarray())
 # b = np.zeros(nt-1)
@@ -78,9 +74,6 @@ def analyticalSol(r, theta, n):
 
 
 
-# ax.contour(R, T, Y-sol,label="Sol fronteiras")
-# ax.plot_surface(R,T, Y-sol)
-
 # A = sol-Y
 # A.reshape(len(sol)**2)
 # L2 = [x**2 for x in A]
@@ -94,9 +87,8 @@ def resolve(numR, numT):
     dr = (1.0 / numR)
     dt = (np.pi / numT)
     A = Ablocks(numR,numT)
-    # matprint(A.toarray())
     b = np.zeros(numT-1)
-    b[-1] = -100./dr**2 * (1.0 + 1./(2.*numR))
+    b[-1] = -100./dr**2 * (1.0 + 1./(2.*(numR+1)))
     v = np.array([])
 
     for i in range(numT-1):
@@ -107,24 +99,24 @@ def resolve(numR, numT):
     Y[1:numR,1:numR] = X
     Y[1:numR, -1] = 100
     return Y
-n = 500
-nr = n
-nt = n
-r = np.linspace(0, 1, nr+1)
-theta = np.linspace(0, np.pi, nt+1)
-R, T = np.meshgrid(r,theta)
-sol =  analyticalSol(R, T, 1000)
-boundary = 100*np.ones(nr+1)
-boundary[0] = 0
-boundary[-1] = 0
-gibbsError = boundary-sol[:,-1]
-plt.plot(r, gibbsError)
-plt.grid()
-plt.show()
+# n = 500
+# nr = n
+# nt = n
+# r = np.linspace(0, 1, nr+1)
+# theta = np.linspace(0, np.pi, nt+1)
+# R, T = np.meshgrid(r,theta)
+# sol =  analyticalSol(R, T, 1000)
+# boundary = 100.0*np.ones(nr+1)
+# boundary[0] = 0
+# boundary[-1] = 0
+# gibbsError = boundary-sol[:,-1]
+# plt.plot(r, gibbsError)
+# plt.grid()
+# plt.show()
 # print(sol[:,nt])
-# h = []
-# l2 = []
-# for i in range(2,10):
+h = []
+l2 = []
+# for i in range(2,9):
 #     n = int(2**i)
 #     nt = n
 #     nr = n
@@ -138,10 +130,45 @@ plt.show()
 #     erro = np.sqrt(np.sum(erro))
 #     h.append(1/nr)
 #     l2.append(erro)
+i = 5
+n = 20
+nt = n
+nr = n
+r = np.linspace(0, 1, nr+1)
+theta = np.linspace(0, np.pi, nt+1)
+R, T = np.meshgrid(r,theta)
+sol =  analyticalSol(R, T, 200)
+X = resolve(nt, nr)
+
+plt.rcParams['text.usetex'] = True
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+# ax = fig.add_subplot()
+
+# ax.contour(R, T, X-sol,label="erro")
+# CS = ax1.contourf(R, T, X)
+ax.set_title("Equação de Laplace em coordenadas Polares",fontsize=16)
+ax.plot_surface(R, T, X,label="Solução por diferenças finitas")
+ax.plot_surface(R, T, sol,label="Solução analítica")
+# plt.subplot(131)
+CS = ax.contourf(R, T, X-sol, zdir='z', offset=-2,cmap=cm.hot)
+cbar = fig.colorbar(CS)
+cbar.ax.set_ylabel('Diferença entre solução analítica e por diferenças finitas')
+
+ax.set_xlabel(r'r',fontsize=12)
+ax.set_ylabel(r'$\theta$',fontsize=12)
+
+# ax.plot_surface(R, T, sol)
+
+# ax.clabel(CS, inline=True, fontsize=10)
+# ax1.set_title('Simplest default with labels')
+# ax.plot_surface(R,T, X)
+
+# ax.plot_surface(R, T, sol)
 
 # plt.loglog(h,l2, "o" , "erro norma L2")
 # plt.loglog(h, np.power(h,2),label="h^2")
 # plt.loglog(h, h, label="h")
-# plt.legend()
-# plt.grid()
-# plt.show()
+plt.legend()
+plt.grid()
+plt.show()
