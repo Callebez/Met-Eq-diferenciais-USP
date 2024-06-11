1164ebc7f9813dba0c84769407a52523bee9b7ff
Callebez <callebe.rocha@outlook.com>
2024-06-10 22:38:36 -0300

    final optimized

--- Optimized/heat.py
+++ Optimized/heat.py
@@ -13,15 +13,15 @@ def laplacian(n):
 def heat_source_uniform(x,y):
     xs = 1.0 / 2.0 
     ys = 1.0 / 2.0
-    w = 0.25
+    w = 0.20
     return np.exp(-((x-xs)**2+(y-ys)**2)/w**2)
 def g(t):
     return 2.0 if t < 0.25 else 0.0
 
 def laplacian_Dirichlet(n, d, k):
     main_diag = -2 * np.ones(n) 
-    main_diag[0] = -d**2#/k
-    main_diag[-1] = -d**2#/k
+    main_diag[0] = -d**2
+    main_diag[-1] = -d**2
     lower_diag =  np.ones(n-1)  
     upper_diag =  np.ones(n-1)  
     upper_diag[0] = 0.0
@@ -78,7 +78,6 @@ def heat_equation_heterogeneous(nx, ny, Lx, Ly, time, dt):
 
     Q = Q.reshape((nx+2, ny+2))
     return X, Y, Q
-
 def laplacian_Neumann(n, d, dt):
     main_diag = -2 * np.ones(n) 
     main_diag[0] =  - 2.0 * d**2
@@ -108,7 +107,6 @@ def heat_equation_uniformNeumann(nx, ny, Lx, Ly, time, dt):
     Ax = eye((nx+2)* (ny+2)) - dt * Dx
     Ay = eye((nx+2)* (ny+2)) - dt * Dy
     A = eye((nx+2)* (ny+2)) - dt * (Dx + Dy)
-    # print(eye((nx+2)* (ny+2)).toarray())
     x = np.linspace(0, Lx, nx + 2)
     y = np.linspace(0, Ly, ny + 2)
     X, Y = np.meshgrid(x, y)
@@ -179,32 +177,32 @@ def heat_equation_deltaNeumann(nx, ny, Lx, Ly, time, dt):
             S[index] = g(t)
 
         ### Using a coupled laplacian      
-        # Q = la.spsolve(A, Q + dt * S)
+        Q = la.spsolve(A, Q + dt * S)
       
         # Q = Q_new
         ### Uncoupled Laplacian     
-        q_new_aux = la.spsolve(Ax, Q + dt / 2.0 * S)
+        # q_new_aux = la.spsolve(Ax, Q + dt / 2.0 * S)
         # S = np.zeros_like(Q)
         # t = i * dt
         # if t < 0.25:
         #     index = (i_s - 1) * nx + (j_s - 1)
         #     S[index] = g(t)
-        Q = la.spsolve(Ay, q_new_aux + dt / 2.0 * S)
+        # Q = la.spsolve(Ay, q_new_aux + dt / 2.0 * S)
         
     Q = Q.reshape((nx+2, ny+2))
     return X, Y, Q
 
 def spatialError(max_power, time_iterations, func):
     # dt = 1./45.
-    dt = 5e-3
+    dt = 5e-1
     n = 3**max_power
     m = math.ceil(n/2)
     total_time = dt*time_iterations
     X, Y, Q = func(n, n, 1.0, 1.0, total_time, dt)
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X,Y,Q)
-    plt.show()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection='3d')
+    # ax.plot_surface(X,Y,Q)
+    # plt.show()
 
     # print(Q[m, m], X[m, m])
     refValue = Q[m, m]
@@ -224,10 +222,10 @@ def temporalError(max_power, total_time, func):
     m = math.ceil(n/2)
     # total_time = dt*time_iterations
     X, Y, Q = func(n, n, 1.0, 1.0, total_time, dt_min)
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X,Y,Q)
-    plt.show()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(projection='3d')
+    # ax.plot_surface(X,Y,Q)
+    # plt.show()
 
     # print(Q[m, m], X[m, m])
     refValue = Q[m, m]
@@ -242,63 +240,61 @@ def temporalError(max_power, total_time, func):
         erro.append(np.abs(Q[m,m] - refValue))
     return h, erro
 
-nx = 21
-ny = 21
-dt = 1e-1
+nx = 91
+ny = 91
+dt = 1e-2
 Lx = 1.0
 Ly = 1.0
-X, Y, Q = heat_equation_uniformNeumann(nx, ny, Lx, Ly, 500.0, dt)
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.plot_surface(X,Y,Q)
+X, Y, Q = heat_equation_deltaNeumann(nx, ny, Lx, Ly, 0.25, dt)
+X1, Y1, Q1 = heat_equation_deltaNeumann(nx, ny, Lx, Ly, 0.26, dt)
+
+
+fig = plt.figure(figsize=plt.figaspect(0.35))
+fig.suptitle("Distribuição de calor pontual \n Condições de fronteira de Neumann")
+# =============
+# First subplot
+# =============
+# set up the Axes for the first plot
+# ax = fig.add_subplot(projection='3d')
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+
+ax.set_title("t = 0.25")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+# ax.set_zticks([])
+# plot a 3D surface like in the example mplot3d/surface3d_demo
+surf = ax.plot_surface(X, Y, Q, rstride=1, cstride=1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+fig.colorbar(surf, shrink=0.5, aspect=10)
+
+# ==============
+# Second subplot
+# ==============
+# set up the Axes for the second plot
+ax = fig.add_subplot(1, 2, 2, projection='3d')
+ax.set_title("t = 0.26")
+ax.set_xlabel("X")
+# ax.set_zticks([])
+ax.set_ylabel("Y")
+# plot a 3D wireframe like in the example mplot3d/wire3d_demo
+surf = ax.plot_surface(X1, Y1, Q1, rstride=1, cstride=1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+fig.colorbar(surf, shrink=0.5, aspect=10)
+
 plt.show()
 
-# X1, Y1, Q1 = heat_equation_heterogeneous(nx, ny, Lx, Ly, 5.0)
-# h, error = temporalError(7, 0.2, heat_equation_uniformNeumann)
-# # h, error = spatialError(7, 10, heat_equation_uniformNeumann)
-# plt.title("Convergência para fonte uniforme \n e condições de Dirichlet ($h = 0.05$)")
-# plt.loglog(h, error, '*')
-# plt.loglog(h, h, label=r"h")
-# plt.loglog(h,np.power(h, 2), label=r"h^2")
-# # plt.loglog(h,np.power(h, 3), label=r"h^3")
-# plt.xlabel(r"$log(dt)$")
-# plt.ylabel(r"$Log(|E|)$ no ponto $(0.5,0.5)$")
+# ## Estudos de convergência.
+# k, errorDt = temporalError(6, 0.2, heat_equation_uniformNeumann)
+# h, errorDx = spatialError(6, 10, heat_equation_uniformNeumann)
+# # plt.title("Convergência para fonte uniforme \n e condições de Dirichlet ($h = 0.05$)")
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 2, 1)
+# ax.loglog(h, errorDx, '*')
+# ax.loglog(h, h, label=r"h")
+# ax1 = fig.add_subplot(1, 2, 2)
+# ax1.loglog(k, errorDt, '*')
+# ax1.loglog(k, k, label=r"k")
+
 # plt.legend()
 # plt.grid()
 # plt.show()
-
-# fig = plt.figure(figsize=plt.figaspect(0.5))
-# fig.suptitle("Distribuição de calor uniforme \n Condições de fronteira de Dirichlet")
-# # =============
-# # First subplot
-# # =============
-# # set up the Axes for the first plot
-# ax = fig.add_subplot(projection='3d')
-# # ax.set_title("t = 0.25")
-# ax.set_xlabel("X")
-# ax.set_ylabel("Y")
-# # ax.set_zticks([])
-# # plot a 3D surface like in the example mplot3d/surface3d_demo
-# surf = ax.plot_surface(X, Y, Q, rstride=1, cstride=1, cmap=cm.coolwarm,
-#                        linewidth=0, antialiased=False)
-# # ax.set_zlim(-1.01, 1.01)
-# fig.colorbar(surf, shrink=0.5, aspect=10)
-
-# # # ==============
-# # # Second subplot
-# # # ==============
-# # # set up the Axes for the second plot
-# # ax = fig.add_subplot(1, 2, 2, projection='3d')
-# # ax.set_title("t = 0.26")
-# # ax.set_xlabel("X")
-# # ax.set_zticks([])
-# # ax.set_ylabel("Y")
-# # # plot a 3D wireframe like in the example mplot3d/wire3d_demo
-# # surf = ax.plot_surface(X1, Y1, Q1, rstride=1, cstride=1, cmap=cm.coolwarm,
-# #                        linewidth=0, antialiased=False)
-# # fig.colorbar(surf, shrink=0.5, aspect=10)
-
-# plt.show()
-
-
-
